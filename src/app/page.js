1e6be65fb5cbea0886e3104f853022e2848732f8
Johns, Erik Michael <erikj3520@gmail.com>
2024-12-05 03:35:30 -0700

    Removed Form Element

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,7 @@ import Homepage from "@/Components/Homepage";
 export default function Home() {
   return (
     <div>
-      <form>
-          <Homepage />
-      </form>
+        <Homepage />
     </div>
   );
 }
